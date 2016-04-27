c8aa865ff9efca70b886df4fe86dd636c8c84d6c
Michal Trněný <trnenmic@fel.cvut.cz>
2016-04-27 23:02:18 +0200

    Database connection established, source of exceptions found and solved.

---
+++ src/main/java/Application/Main.java
@@ -0,0 +1,33 @@
+package Application;
+
+import Model.Airplane;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ * Created by M on 27. 4. 2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("NewPersistenceUnit");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+
+        /*Airplane airplane = Airplane.createAirplane(50, 25, "AIRLINE", 5, 10, "CODE_123");
+
+        tx.begin();
+        em.persist(airplane);
+        tx.commit();*/
+
+        Airplane airplane = em.find(Airplane.class, 101);
+        System.out.println(airplane);
+
+//finding by ID
+        //Airplane booka = em.find(Airplane.class, 1);
+        //System.out.println("Finding book> "+booka);
+    }
+}
